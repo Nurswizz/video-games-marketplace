f038aec4d1ab2411518aabf0a32afc838eb25d53
Nurswizz <nur646132@gmail.com>
2026-01-19 01:58:03 +0500

    testing

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -1,7 +1,37 @@
 package src;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+import src.config.Database;
+
 public class Main {
     public static void main(String[] args) {
+        Connection conn = null;
+        Scanner sc = new Scanner(System.in);
 
+        try {
+            conn = new Database().getConnection();
+            System.out.println("Connected to database successfully");
+
+            while (true) {
+                System.out.println("Welcome to VideoGames Market");
+                System.out.println("-----------");
+                System.out.println("Write your choice");
+                System.out.println("/quit");
+                String choice = sc.nextLine();
+                if (choice.equalsIgnoreCase("/quit")) {
+                    System.out.println("Thanks for playing!");
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
